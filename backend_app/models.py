9b75b56c84543649e81a8f6d8972ca44d0caf0f4
SanketDawange <sanketdawange6@gmail.com>
2024-02-28 11:11:55 +0530

    Fixed not null columns in db

--- backend_app/models.py
+++ backend_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class SensorData(models.Model):
     created_at = models.DateTimeField(primary_key=True)
-    entry_id = models.IntegerField()
-    field1 = models.IntegerField()
-    field2 = models.FloatField()
-    field3 = models.FloatField()
+    entry_id = models.IntegerField(null=True, blank=True)
+    field1 = models.IntegerField(null=True, blank=True)
+    field2 = models.FloatField(null=True, blank=True)
+    field3 = models.FloatField(null=True, blank=True)
     field4 = models.FloatField(null=True, blank=True)
